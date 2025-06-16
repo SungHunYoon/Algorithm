978577b815e9d60ee58614122c16c21e5e4d712c
SungHunYoon <gnsals6004@gmail.com>
2025-06-16 16:16:35 +0900

    Add: Today's algorithm problem

---
+++ 42seoul/23291.cpp
@@ -0,0 +1,291 @@
+#include <iostream>
+#include <set>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
+using iiii = tuple<int, int, int, int>;
+
+int N, K;
+int A[101][101];
+int B[101][101];
+int T[101][101];
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+void printArr() {
+	cout << "========= printArr \n";
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << A[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+}
+
+void printB() {
+	cout << "========= printB \n";
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << B[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+}
+
+void printT() {
+	cout << "======== printT \n";
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << T[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+}
+
+void clearA() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+			A[i][j] = 0;
+	}
+}
+
+void clearB() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+			B[i][j] = 0;
+	}
+}
+
+void clearT() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+			T[i][j] = 0;
+	}
+}
+
+int countCol(int n) {
+	int cnt = 0;
+	for (int i = 0; i < N; i++) {
+		if (A[i][n]) cnt++;
+	}
+	return cnt;
+}
+
+int countTCol(int n) {
+	int cnt = 0;
+	for (int i = 0; i < N; i++) {
+		if (T[i][n]) cnt++;
+	}
+	return cnt;
+}
+
+void increaseArr() {
+	int mCnt = 1e9;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (A[i][j] == 0) continue;
+			mCnt = min(mCnt, A[i][j]);
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (A[i][j] == mCnt)
+				A[i][j]++;
+		}
+	}
+}
+
+void rotateArr() {
+	int left = A[0][0];
+	
+	for (int i = 1; i < N; i++) {
+		A[0][i - 1] = A[0][i];
+	}
+	A[0][N - 1] = 0;
+	A[1][0] = left;
+	
+	int R = N;
+	while (1) {
+		clearB();
+		clearT();
+		int cut = 0;
+		int maxCol = 0;
+		for (int i = 0; i < N; i++, cut++) {
+			int col = countCol(i);
+			if (col < 2) break;
+			maxCol = max(maxCol, col);
+			for (int j = 0; j < col; j++) {
+				B[j][i] = A[j][i];
+			}
+		}
+		if (R - cut < maxCol) break;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				T[N - j - 1][i] = B[i][j];
+			}
+		}
+		int idx = 0;
+		for (int i = cut, j = 0; i < N && A[0][i]; i++, j++, idx++) {
+			B[0][j] = A[0][i];
+		}
+		while (T[0][0] == 0) {
+			for (int i = 1; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					T[i - 1][j] = T[i][j];
+					T[i][j] = 0;
+				}
+			}
+		}
+		for (int i = 1; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				B[i][j] = T[i - 1][j];
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				A[i][j] = B[i][j];
+			}
+		}
+		R = idx;
+	}
+}
+
+void calDiff() {
+	set<iiii> S;
+	clearT();
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (!A[i][j]) continue;
+			for (int k = 0; k < 4; k++) {
+				int nx = i + dx[k];
+				int ny = j + dy[k];
+				if (0 > nx || nx >= N || 0 > ny || ny >= N) continue;
+				if (!A[nx][ny]) continue;
+				if (S.find({i, j, nx, ny}) != S.end()) continue;
+				S.insert({i, j, nx, ny});
+				S.insert({nx, ny, i, j});
+				int d = abs(A[i][j] - A[nx][ny]) / 5;
+				if (d == 0) continue;
+				if (A[i][j] > A[nx][ny]) {
+					T[i][j] -= d;
+					T[nx][ny] += d;
+				} else {
+					T[i][j] += d;
+					T[nx][ny] -= d;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			A[i][j] += T[i][j];
+		}
+	}
+}
+
+void serialize() {
+	clearB();
+	int idx = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (!A[j][i]) continue;
+			B[0][idx++] = A[j][i];
+		}
+	}
+	clearA();
+	for (int i = 0; i < N; i++) {
+		A[0][i] = B[0][i];
+	}
+}
+
+void divideArr() {
+	int R = N;
+	int P = 2;
+	while (P--) {
+		int pvt = R / 2;
+		clearB();
+		clearT();
+		int col = countCol(0);
+		for (int k = 0; k < col; k++) {
+			for (int i = 0; i < pvt; i++)
+				B[k][i] = A[k][i];
+		}
+		for (int i = 0; i < col; i++) {
+			for (int j = 0; j < pvt; j++) {
+				T[col - i - 1][pvt - j - 1] = B[i][j];
+			}
+		}
+		while (T[0][0] == 0) {
+			for (int i = 1; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					T[i - 1][j] = T[i][j];
+					T[i][j] = 0;
+				}
+			}
+		}
+		clearB();
+		int idx = 0;
+		for (int k = 0; k < col; k++) {
+			for (int i = pvt, j = 0; i < N; i++, j++)
+				B[idx][j] = A[k][i];
+			idx++;
+		}
+		for (int k = 0; k < col; k++) {
+			for (int i = 0, j = 0; i < pvt; i++, j++)
+				B[idx][j] = T[k][i];
+			idx++;
+		}
+		clearA();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				A[i][j] = B[i][j];
+			}
+		}
+		R = pvt;
+	}
+}
+
+int minCount() {
+	int cnt = 1e9;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (!A[i][j]) continue;
+			cnt = min(cnt, A[i][j]);
+		}
+	}
+	return cnt;
+}
+
+int maxCount() {
+	int cnt = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+			cnt = max(cnt, A[i][j]);
+	}
+	return cnt;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> N >> K;
+	for (int i = 0; i < N; i++) {
+		cin >> A[0][i];
+	}
+	int answer = 0;
+	while (maxCount() - minCount() > K) {
+		increaseArr();
+		rotateArr();
+		calDiff();
+		serialize();
+		divideArr();
+		calDiff();
+		serialize();
+		answer++;
+	}
+	cout << answer << '\n';
+	return 0;
+}
